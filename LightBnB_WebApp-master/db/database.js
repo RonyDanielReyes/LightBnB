ede5434448b8ee54f540a5adc0c4ca13c75bd1d9
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-11-24 00:18:28 -0800

    LighthouseBnB users

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,16 +1,16 @@
+const { Pool } = require('pg');
+
+const pool = new Pool( {
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+})
+
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
 /// Users
-
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'labber',
-  password: 'labber',
-  host: 'localhost',
-  database: 'lightbnb'
-}); 
 
 /**
  * Get a single user from the database given their email.
@@ -18,14 +18,30 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  // let resolvedUser = null;
+  // for (const userId in users) {
+  //   const user = users[userId];
+  //   if (user && user.email.toLowerCase() === email.toLowerCase()) {
+  //     resolvedUser = user;
+  //   }
+  // }
+  // return Promise.resolve(resolvedUser);
+
+  const queryString = `
+  SELECT * FROM users
+  WHERE email = $1`
+
+  return pool
+    .query(queryString, [email])
+    .then((result) => {
+      if (!result.rows[0]) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
 };
 
 /**
@@ -34,7 +50,21 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT * FROM users
+  WHERE id = $1`;
+
+  return pool
+    .query(queryString, [id])
+    .then((result) => {
+      if (!result.rows[0]) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
 };
 
 /**
@@ -43,10 +73,30 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *`;
+
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+
+  return pool
+    .query(queryString, [name, email, password])
+    .then((result) => {
+      // console.log(result.rows[0])
+      return result.rows[0]
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
+
 };
 
 /// Reservations
@@ -68,16 +118,25 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = function (options, limit = 10) {
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
+
+  const queryString = `
+  SELECT * FROM properties
+  LIMIT $1;`
+
+  return pool.query(queryString, [limit])
+  .then((result) => {
+    // console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log('error:', err.message);
+  });
 };
 
 /**
